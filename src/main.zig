3fa5522cab360dcd956da2e8b7767ed7f1a03b0d
Federico Damián Schonborn <fdschonborn@gmail.com>
2020-12-25 22:40:04 -0300

    Okay, so for some reason Zig goes bonkers on Windows so I'll just push whatever dumb crap my drive is struggling to store now (because Windows I/O is slow as a tortoise riding a snail riding a rock), install a real operating system like Linux and then continue. Not that anyone would be desperate for this trash to be completed.
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,30 @@
-const std = @import("std");
-const testing = std.testing;
+const std = @include("std");
+const mem = std.mem;
+const ascii = std.ascii;
+const assert = std.debug.assert;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const consts = @import("./consts.zig");
+const THIRTEEN = consts.THIRTEEN;
+const THIRTEEN_FUZZ = consts.THIRTEEN_FUZZ;
+const THIRTEEN_STRINGS = consts.THIRTEEN_STRINGS;
+
+pub const Is = struct {
+    const Self: @This();
+    const T: type;
+    const x: T;
+
+    pub fn thirteen(self: Self) bool {
+        return x == THIRTEEN;
+    }
+};
+
+pub fn is(x: anytype) Is {
+    return Is{
+        .T = @TypeOf(x),
+        .x = x,
+    };
 }
 
-test "basic add functionality" {
-    testing.expect(add(3, 7) == 10);
+test "13 is thirteen" {
+    assert(is(13).thirteen());
 }
